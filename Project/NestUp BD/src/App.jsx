a6395ded47e77450ffdf7417e40604429582c99f
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2025-06-13 18:52:06 +0600

    synced  the app jsx

--- Project/NestUp BD/src/App.jsx
+++ Project/NestUp BD/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Search from './pages/Search/Search';
 import './App.css';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <div className="app">
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* Other routes will be added later */}
+          <Route path="/search" element={<Search />} />
           <Route path="*" element={<Home />} /> {/* Temporary fallback */}
         </Routes>
       </div>
