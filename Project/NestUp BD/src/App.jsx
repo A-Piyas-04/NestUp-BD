4aa8ae7e3238f97e4588d22dcf3e8f0ad3adb75a
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2025-06-22 22:38:49 +0600

    remove unused search feature components

--- Project/NestUp BD/src/App.jsx
+++ Project/NestUp BD/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
 import './App.css';
 
 function App() {
@@ -9,7 +8,7 @@ function App() {
       <div className="app">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
+          {/* Other routes will be added later */}
           <Route path="*" element={<Home />} /> {/* Temporary fallback */}
         </Routes>
       </div>
