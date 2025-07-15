946342dd25c19530c5d4760d4474b105dcaf4d18
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2025-07-15 19:45:36 +0600

    updatedt filter options ins earch page

--- Project/NestUp BD/src/components/FilterSidebar/FilterSidebar.jsx
+++ Project/NestUp BD/src/components/FilterSidebar/FilterSidebar.jsx
@@ -4,7 +4,7 @@ import './FilterSidebar.css';
 const FilterSidebar = () => {
   return (
     <div className="filter-sidebar">
-      <h3>Filter Services</h3>
+      {/* <h3>Filter Services</h3>
       <label>
         Service Type:
         <select>
@@ -13,11 +13,45 @@ const FilterSidebar = () => {
           <option>Transport</option>
           <option>Food</option>
         </select>
+      </label> */}
+
+ <label>
+        Division:
+        <select>
+          <option value="">All Divisions</option>
+          <option value="Dhaka">Dhaka Division</option>
+          <option value="Chittagong">Chittagong Division</option>
+          <option value="Rajshahi">Rajshahi Division</option>
+          <option value="Khulna">Khulna Division</option>
+          <option value="Barisal">Barisal Division</option>
+          <option value="Sylhet">Sylhet Division</option>
+          <option value="Rangpur">Rangpur Division</option>
+          <option value="Mymensingh">Mymensingh Division</option>
+        </select>
       </label>
 
       <label>
-        Location:
-        <input type="text" placeholder="Enter area..." />
+        District:
+        <select>
+          <option value="">All Districts</option>
+          <option value="Dhaka">Dhaka</option>
+          <option value="Gazipur">Gazipur</option>
+          <option value="Narayanganj">Narayanganj</option>
+          <option value="Chittagong">Chittagong</option>
+          <option value="Cox's Bazar">Cox's Bazar</option>
+          <option value="Rajshahi">Rajshahi</option>
+          <option value="Khulna">Khulna</option>
+          <option value="Barisal">Barisal</option>
+          <option value="Sylhet">Sylhet</option>
+          <option value="Rangpur">Rangpur</option>
+          <option value="Mymensingh">Mymensingh</option>
+          {/* Add more districts as needed */}
+        </select>
+      </label>
+
+      <label>
+        Search Area:
+        <input type="text" placeholder="Enter local area..." />
       </label>
 
       <label>
