2ab6dac918f091ec597de4cd13bf5e7e1bf92155
irfanshafee <irfanshafee1@gmail.com>
2025-08-23 23:39:32 +0600

    added index.js file and booking approval css file

---
+++ Project/NestUp BD/Client/src/pages/Dashboard/views/bookings/index.js
@@ -0,0 +1,2 @@
+export { default as BookedNests } from './BookedNests';
+export { default as BookingApprovals } from './BookingApprovals';
---
+++ Project/NestUp BD/Client/src/pages/Dashboard/views/nests/index.js
@@ -0,0 +1,2 @@
+export { default as MyNests } from './MyNests';
+export { default as Wishlist } from './Wishlist';
